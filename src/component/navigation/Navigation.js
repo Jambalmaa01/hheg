c4140a01dabf5dbd04eb374e999e6d1925428a5f
jambalmaa <njambalmaa0506@gmail.com>
2025-07-01 01:32:54 +0800

    xvx

--- src/component/navigation/Navigation.js
+++ src/component/navigation/Navigation.js
@@ -3,6 +3,7 @@ import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from '@react-navigation/native-stack'; 
 import FileTable from '../table/file/FIleTable'
 import UserTable from '../table/userTable/UserTable'
+import Dashboard from '../screens/dashboard/Dashboard'
 
 const Stack = createNativeStackNavigator();
 
@@ -10,6 +11,7 @@ export default function Navigation(){
     return(
        <NavigationContainer>
         <Stack.Navigator initialRouteName="Home">
+            <Stack.Screen name="Dashboard" component={Dashboard} />
           <Stack.Screen name="FileTable" component={FileTable} />
           <Stack.Screen name="UserTable" component={UserTable} />
         </Stack.Navigator>
--- src/component/screens/home/Home.js
+++ src/component/screens/home/Home.js
@@ -1,25 +1,47 @@
-import { Box } from "@mui/material";
+import { Box, Grid, Button } from "@mui/material";
 import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 import FileTable from '../../table/file/FIleTable';
 import UserTable from '../../table/userTable/UserTable';
+import Dashboard from '../dashboard/Dashboard'
+import DashboardIcon from '@mui/icons-material/Dashboard';
 
 export default function Home() {
     return (
         <BrowserRouter>
-            <Box>
-                <Box id='Box0'>
-                    <Box id='dvi01'>
-                        <Link to="/">Үндэсэн өгөгдэхүүн</Link>
-                        <Link to="/UserTable">Хэрэглэгч</Link>
-                        <Link to="/FileTable">Файл</Link>
+        <Grid container direction="column" sx={{ height: '100vh' }}>
+            <Grid item>
+               <Box sx={{ width: '100%', height: 50, backgroundColor: 'gray' }}></Box>
+            </Grid>
+
+            <Grid item sx={{ flex: 1 }} container>
+                <Grid item>
+                    <Box display="flex" flexDirection='column' alignItems='flex-start' sx={{ width: 200, height: '100%', border:1, borderColor: 'gray', a: { textDecoration: 'none', color: 'inherit' }, }} >
+                        <Button>
+                            <DashboardIcon/>
+                        <Link to="/Dashboard">Үндэсэн өгөгдэхүүн</Link>
+
+                        </Button>
+                        <Button>
+                            <DashboardIcon/>
+                        <Link to="/UserTable"> Хэрэглэгч</Link>
+
+                        </Button>
+                        <Button>
+                            <DashboardIcon/>
+                        <Link  to="/FileTable">Файл</Link>
+
+                        </Button>
                     </Box>
-                </Box>
-                <Box id='Box1'></Box>
+                </Grid>
+                <Grid item xs sx={{ padding: 2 }}>
                 <Routes>
+                    <Route  path="/Dashboard" element={<Dashboard />} />
                     <Route path="/UserTable" element={<UserTable />} />
                     <Route path="/FileTable" element={<FileTable />} />
                 </Routes>
-            </Box>
+                </Grid>
+            </Grid>
+            </Grid>
         </BrowserRouter>
     );
-}
+}                        
